1b31ece4f2b56199769f514afd7637845c003eb8
annie .h.h <annie2016l@gmail.com>
2019-02-17 00:11:01 +0400

    SqlTransaction

---
+++ Transaction/Transaction/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Transaction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = @"Data Source=(localdb)\MSSQLLocalDB; Initial Catalog=DBforJoin; Integrated Security=True";
+
+            try
+            {
+                using (SqlConnection sqlConnection = new SqlConnection(connectionString)) // create connection
+                {
+                    string sqlQuery1 = "UPDATE Companies SET [Company name] = 'Coal Mining LLC' WHERE [Type of activity] = 'Mining of hard coal'"; // create a query
+
+                    using (SqlCommand command1 = new SqlCommand(sqlQuery1, sqlConnection)) // create a command
+                    {
+                        sqlConnection.Open();  // Open() - method of SqlConnection, which opens a database connection.
+
+                        command1.Transaction = sqlConnection.BeginTransaction();  // BeginTransaction() - method of SqlConnection, which starts a database transaction.
+                        command1.ExecuteNonQuery(); // ExecuteNonQuery() - method of SqlCommand, which executes a T-SQL statement against the connection and returns the number of rows affected.
+
+                        command1.Transaction.Rollback();  // Rollback() - method of SqlTransaction, which rolls back a transaction from a pending state.
+                        Console.WriteLine("Transaction rollback");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ Transaction/Transaction2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Transaction2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionStr = @"Data Source=(localdb)\MSSQLLocalDB; Initial Catalog=DBforJoin; Integrated Security=True";
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionStr))
+                {
+                    connection.Open();
+
+                    using (SqlCommand sqlCommand = new SqlCommand("SELECT * FROM Companies", connection))
+                    {
+                        sqlCommand.Transaction = connection.BeginTransaction(IsolationLevel.ReadCommitted);  // Starts a database transaction with the specified isolation level.
+                                                                                                             // By this isolation level transaction2 will wait until transaction1 ends and then it will read and print all the data written by query
+
+                        using (SqlDataReader sqlDataReader = sqlCommand.ExecuteReader())
+                        {
+                            while (sqlDataReader.Read())
+                            {
+                                if (sqlDataReader[2] == DBNull.Value) // check if exists any member equals null
+                                {
+                                    Console.WriteLine(sqlDataReader.GetSqlValue(0) + ". " +
+                                        sqlDataReader.GetString(1) + " - there is no activity");
+                                }
+                                else
+                                {
+                                    Console.WriteLine(sqlDataReader.GetSqlValue(0) + ". " +
+                                        sqlDataReader.GetString(1) + " - type of activity - " + sqlDataReader.GetString(2));
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
